cec5951336fe88b4b8db3ee90afc5ec6a46dde10
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-03 18:51:37 -0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,10 @@ import { Routes } from '@angular/router';
 import { authGuard, authRedirectGuard } from '@shared/guards/auth-guard';
 
 export const routes: Routes = [
-
   {
     path: 'auth',
     canActivate: [authRedirectGuard],
-    loadChildren: () => import('./features/auth/auth.routes')
+    loadChildren: () => import('./features/auth/auth.routes'),
   },
   {
     path: '',
@@ -15,40 +14,56 @@ export const routes: Routes = [
     children: [
       {
         path: 'dashboard',
-        loadComponent: () => import('@features/dashboard/dashboard')
+        loadComponent: () => import('@features/dashboard/dashboard'),
+        data: { breadcrumb: 'Dashboard', icon: 'icon-[fa7-solid--layer-group]' },
       },
-
       {
         path: 'clientes',
         children: [
-          { path: '', loadComponent: () => import('./features/customers/list/customers-list') },
-          { path: 'create', loadComponent: () => import('./features/customers/create/customers-create') },
+          {
+            path: '',
+            loadComponent: () => import('./features/customers/list/customers-list'),
+          },
+          {
+            path: 'create',
+            loadComponent: () => import('./features/customers/create/customers-create'),
+          },
           // { path: 'edit/:id', loadComponent: () => import('./features/customers/edit/customers-edit') },
-        ]
+        ],
       },
       {
         path: 'tickets',
-        loadComponent: () => import('@features/tickets/tickets')
+        data: { breadcrumb: 'Tickets', icon: 'icon-[fa7-solid--ticket]' },
+        loadComponent: () => import('@features/tickets/tickets'),
       },
       {
         path: 'configuracion',
+        data: { breadcrumb: 'Configuración', icon: 'icon-[fa7-solid--cog]' },
         children: [
-          { path: '', loadComponent: () => import('@features/settings/settings') },
+          {
+            path: '',
+            loadComponent: () => import('@features/settings/settings'),
+          },
           {
             path: 'crear_usuario',
-            loadComponent: () => import('@features/auth/sign-up/sign-up')
+            loadComponent: () => import('@features/auth/sign-up/sign-up'),
+            data: { breadcrumb: 'Crear Usuario', icon: 'icon-[fa7-solid--user-plus]' },
+          },
+          {
+            path: 'tiendas',
+            loadComponent: () => import('@features/shops/shops'),
+            data: { breadcrumb: 'Tiendas', icon: 'icon-[fa7-solid--store]' },
           }
-        ]
+        ],
       },
       {
         path: '**',
-        redirectTo: 'dashboard'
-      }
-    ]
+        redirectTo: 'dashboard',
+      },
+    ],
   },
   {
     path: '**',
     redirectTo: 'auth',
-  }
-
+  },
 ];
